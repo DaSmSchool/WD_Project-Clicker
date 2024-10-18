70a6d13f64834ec9d929cad3519b5c9566ce9f21
DaSmSchool <dariuszs2@nycstudents.net>
2024-10-18 15:11:22 -0400

    add student hiring

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 var clickTimes = 0
+var studentsEntered = 0
 
 const countEl = document.getElementById("count-el")
+const prevEl = document.getElementById("prev-el")
+const hiredEl = document.getElementById("hired-el")
+const hiredCount = document.getElementById("hired-count")
 
 function increment() {
+    studentsEntered += 1
+    countEl.innerText = studentsEntered
+    console.log(studentsEntered)
+    setTimeout(() => {
+        increment()
+    }, 1000);
+}
+
+function incr_clicked() {
     clickTimes += 1
-    countEl.innerText = clickTimes
-    console.log(clickTimes)
+    hiredCount.innerText = "" + clickTimes
+    increment()
+}
+
+function save() {
+    prevEl.innerText += clickTimes + "-"
 }
 
 function init() {
     countEl.innerText = 0
+    prevEl.innerText += "\n"
 }
 
 init()
